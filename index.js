467490eb5acec6b5076f2ac392918f0ccbbba3a9
farzanapomy <farzanapomy56@gmail.com>
2021-11-12 01:27:46 +0600

    added

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
+// middleware 
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fjoai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
 console.log(uri);
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('puppiesShop')
+        const productsCollections = database.collection('products');
+
+
+        // post product 
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollections.insertOne(product)
+            // console.log(result);
+            res.json(result)
+        })
+
+
+        // post products 
+        app.get('/products', async (req, res) => {
+            const cursor=productsCollections.find({});
+            const products=await cursor.toArray();
+            res.send(products);
+        })
+
+
+    }
+
+    finally {
+        // await client.close()
+    }
+}
+
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello !! server is live');
